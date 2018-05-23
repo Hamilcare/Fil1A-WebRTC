7bbfaaeb6b38bcac85259215117780059b561c15
Cédric <CedricG44@users.noreply.github.com>
2018-05-23 13:44:04 +0200

    Solution UWP

---
+++ Serveur/TheiaVR-WebRTC/Test/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+using Envoi;
+using Reception;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,1 @@
 # Kinect to WebRTC
-![Travis-build-status](https://travis-ci.org/Hamilcare/Fil1A-WebRTC.svg?branch=master)
