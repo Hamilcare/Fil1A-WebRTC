c2f5408e0cddc12c1a03a0f69cb720bd0a149498
Louradour Vivien <27066260+vivienlouradour@users.noreply.github.com>
2018-06-02 16:35:29 +0200

    add simple GUI+Console, Start logic

---
+++ ServeurFusion/ServeurFusion.Core/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ServeurFusion.EnvoiRTC;
+using ServeurFusion.ReceptionUDP;
+using ServeurFusion.ReceptionUDP.Datas;
+using ServeurFusion.ReceptionUDP.Datas.PointCloud;
+using ServeurFusion.ReceptionUDP.TransformationServices;
+using ServeurFusion.ReceptionUDP.UdpListeners;
+
+namespace ServeurFusion.Core
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Structures de données 
+        private DataTransferer<Skeleton> skeletonUdpToMiddle = new DataTransferer<Skeleton>();
+        private DataTransferer<Skeleton> skeletonMiddleToWebRtc = new DataTransferer<Skeleton>();
+
+        private DataTransferer<Cloud> cloudUdpToMiddle = new DataTransferer<Cloud>();
+        private DataTransferer<Cloud> cloudMiddleToWebRtc = new DataTransferer<Cloud>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnShowConsole_Click(object sender, RoutedEventArgs e)
+        {
+            Program.ShowConsole();
+        }
+
+        private void BtnHideConsole_Click(object sender, RoutedEventArgs e)
+        {
+            Program.HideConsole();
+        }
+
+        private void BtnCustomDebug_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("hello world !");
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            //Désactive le bouton start
+            BtnStart.IsEnabled = false;
+            BtnStop.IsEnabled = true;
+
+            var skeletonUdpListener = new UdpSkeletonListener(skeletonUdpToMiddle, 9877);
+            var cloudUdpListener = new UdpCloudPointListener(cloudUdpToMiddle, 9876);
+            var skeletonTransformationService = new TransformationSkeletonService(skeletonUdpToMiddle, skeletonMiddleToWebRtc);
+            var cloudTransformationService = new TransformationCloudPointService(cloudUdpToMiddle, cloudMiddleToWebRtc);
+
+            WebRtcCommunication webRtcSender = new WebRtcCommunication(skeletonMiddleToWebRtc, cloudMiddleToWebRtc);
+
+            skeletonUdpListener.Listen();
+            skeletonTransformationService.Prosecute();
+
+            cloudUdpListener.Listen();
+            cloudTransformationService.Prosecute();
+
+            webRtcSender.Connect();
+            //Console.ReadLine();
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            BtnStart.IsEnabled = true;
+            BtnStop.IsEnabled = false;
+        }
+    }
+}
--- ServeurFusion/ServeurFusion.Core/Program.cs
+++ ServeurFusion/ServeurFusion.Core/Program.cs
@@ -7,17 +7,65 @@ using ServeurFusion.ReceptionUDP.TransformationServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ServeurFusion.Core
 {
+    
     class Program
     {
+        #region console logic
+        //Cette région comporte la logique d'affichage de la console (permet d'afficher et de masquer la console sur l'appli WPF
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AllocConsole(); // Create console window
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow(); // Get console window handle
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        public static void ShowConsole()
+        {
+            var handle = GetConsoleWindow();
+            if (handle == IntPtr.Zero)
+                AllocConsole();
+            else
+                ShowWindow(handle, SW_SHOW);
+        }
+
+        public static void HideConsole()
+        {
+            var handle = GetConsoleWindow();
+            if (handle != null)
+                ShowWindow(handle, SW_HIDE);
+        }
+        #endregion
+
+        public static Application WinApp { get; private set; }
+        public static Window MainWindow { get; private set; }
+        
+
+        static void InitializeWindows()
+        {
+            WinApp = new Application();
+            WinApp.Run(MainWindow = new MainWindow()); // note: blocking call
+        }
+
+        [STAThread]    
         static void Main(string[] args)
         {
-            Test();
+            //Launch GUI
+            InitializeWindows(); // opens the WPF window and waits here
         }
+
+        /*
         private static void Test()
         {
             DataTransferer<Skeleton> skeletonUdpToMiddle = new DataTransferer<Skeleton>();
@@ -43,5 +91,7 @@ namespace ServeurFusion.Core
             webRtcSender.Connect();
             Console.ReadLine();
         }
+        */
     }
+
 }
