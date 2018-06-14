b294667afdd56228df690300df240691b63109c7
vquiedeville <valentin.quiedeville@soprasteria.com>
2018-06-14 16:40:33 +0200

    remove log

--- ClientWeb/reborn.js
+++ ClientWeb/reborn.js
@@ -1,5 +1,3 @@
-
-
 function init(){
 
     renderer = new THREE.WebGLRenderer();
@@ -10,8 +8,7 @@ function init(){
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 10000 );
-    //camera.position.set(0, 0, -3);
-    //camera.position.set(0, 0, -1000);
+
 	camera.position.set(0, 0, -5);
     camera.lookAt(scene.position);
 
@@ -25,28 +22,18 @@ function init(){
 
 var currentTimestamp; //currentTimestamp
 var cloudDataBuffer = "";  //current data buffer
-//var arrayOfDataReadyToRender = []; //array of data ready to be process by rendering method
 var isRenderingReady = true; //true if rendering method is ready to process a new set of data
 var timestampLength;
 
 //Store cloud data while timestamp is the same
 function handleCloudData(data){
 	
-	//console.log(data);
-	//console.log(arrayOfDataReadyToRender.length);
-	//if(!timestampLength){
-		timestampLength = data.indexOf(';');
-	//}
-	//console.log(timestampLength);
+	timestampLength = data.indexOf(';');
+
 	trameTimestamp = Number(data.substring(0,timestampLength));
 	if(!currentTimestamp)//sur la première trame reçue
 		currentTimestamp=trameTimestamp;
-	
-	//console.log("currentTimestamp");
-	//console.log(currentTimestamp);
-	
-	//console.log("trameTimestamp");
-	//console.log(trameTimestamp);
+
 	if(trameTimestamp == currentTimestamp){
 		cloudDataBuffer += data.substring(timestampLength);
 	}
@@ -57,15 +44,12 @@ function handleCloudData(data){
 		currentTimestamp = trameTimestamp;
 	}
 	
-	//if(isRenderingReady && arrayOfDataReadyToRender.length>0){
-	//	synchronousCloud(arrayOfDataReadyToRender.shift());
-	//}
 }
 
 function parseColorFromData(data){
 	var arrayOfCoordinates = data.split(";");
 	while((arrayOfCoordinates.length)%6 !=0){
-		arrayOfCoordinates.push("0");
+		arrayOfCoordinates.push("255");
 	}
 	var array = new Float32Array(arrayOfCoordinates.length/2);
 	var index = 0;
@@ -81,38 +65,26 @@ function parseColorFromData(data){
 }
 
 function parseCoordinatesFromData(data){
-	//console.log("unsplit data one frame");
-	//console.log(data);
+
 	var arrayOfCoordinates = data.split(";");
 	while((arrayOfCoordinates.length)%6 !=0){
 		arrayOfCoordinates.push("0");
 	}
 	
-	//console.log("data for one frame");
-	//console.log(arrayOfCoordinates);
 	var array = new Float32Array(arrayOfCoordinates.length/2);
 	var index = 0;
 	for(let i=0; i< arrayOfCoordinates.length-6; i=i+6){
 		//X
 		array[index] = parseFloat(arrayOfCoordinates[i]) || 0;
-		if(isNaN(array[index])){
-			console.log("nan detected"+index);
-			array[index] = 0;
-		}
+
 		index++;
 		//Y
 		array[index] = parseFloat(arrayOfCoordinates[i+1])|| 0;
-		if(isNaN(array[index])){
-			console.log("nan detected"+index);
-			array[index] = 0;
-		}
+
 		index++;
 		//Z
 		array[index] = parseFloat(arrayOfCoordinates[i+2])|| 0;
-		if(isNaN(array[index])){
-			console.log("nan detected "+index);
-			array[index] = 0;
-		}
+
 		index++;
 	}
 	
@@ -121,7 +93,7 @@ function parseCoordinatesFromData(data){
 
 
 function synchronousCloud(data){
-	console.log("Starting rendering cubes using one synchronousCloud");
+	//console.log("Starting rendering cubes using one synchronousCloud");
 	//Starting new rendering so not ready to process another set of data
 	isRenderingReady = false;
 	var material = new THREE.PointsMaterial({
@@ -137,8 +109,8 @@ function synchronousCloud(data){
 	var promiseParseCoordinate = new Promise(function(resolve,reject){
 		parsedData = parseCoordinatesFromData(data)
 		resolve(parsedData);
-		console.log("J'ai parse les coord");
-		console.log(parsedData);
+		//console.log("J'ai parse les coord");
+		//console.log(parsedData);
 		return parsedData;
 	});
 	//On parse ensuite les couleurs
@@ -165,7 +137,7 @@ function synchronousCloud(data){
 		myScene.add(cloudPoints);
 		renderer.render( myScene, camera );
 		endMethod = performance.now();
-		console.log("Method took "+(endMethod - startMethod)+"ms");
+		//console.log("Method took "+(endMethod - startMethod)+"ms");
 		
 		//Rendering is finished, method is ready to process new set of data
 		isRenderingReady = true;
